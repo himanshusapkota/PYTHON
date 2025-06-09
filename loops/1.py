e73ff85bb9d55560197788831c00d8f074198cab
himanshuspkt <spkthimanshu2010@gmail.com>
2025-06-09 19:39:14 +0545

    py

---
+++ loops/1.py
@@ -0,0 +1,26 @@
+# # USING ELSE IN PY with for loop
+
+# for i in range (5):
+#     print(i)
+    
+    
+# else:
+#     print("SORRY BHAI")    
+    
+# # 0
+# # 1
+# # 2
+# # 3
+# # 4
+# # SORRY BHAI
+
+# for i in range (6):
+#     print(i)
+#     if i==4:
+#         break
+    
+# else:
+#     print("Sorry")
+        
+    
+    
