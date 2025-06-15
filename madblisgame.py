6df5bb5c024626c79efa58b8633fe0c6dda59067
himanshusapkota <himanshuspkt@gmail.com>
2025-06-15 08:46:14 +0545

    game

---
+++ madblisgame.py
@@ -0,0 +1,18 @@
+
+
+# madlibs Game
+#making a story by filling the space
+#fun game
+
+adjective1 = input(str("Enter an Adjective : "))
+noun1 = input(str("Enter a Noun : "))
+adjective2 = input(str("Enter an Adjective : "))
+verb1 =  input(str("Enter a Verb : "))
+adjective3 = input(str("Enter an Adjective : "))
+
+
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In an exibit i saw a {noun1}")
+print(f"{noun1}was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
